ebd5854ec9b66fac533aeff9eba243238f520268
김형석 <kimhyoungseok@gimhyeongseog-ui-MacBookPro.local>
2025-04-27 23:59:33 +0900

    누나라우터연습

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Homepage from './page/Homepage';
+import Aboutpage from './page/Aboutpage';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/about" element={<Aboutpage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/page/Homepage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+const Homepage = () => {
+  return <div>
+    <h1>Homepage</h1>
+    <Link to="/about">Go to about page</Link>
+  </div>;
+};
+
+export default Homepage;
